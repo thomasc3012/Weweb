28695e117696ed41657ab790bc2d78cf274dc707
Thomas Van epen (via WeWeb) <thomasve@live.nl>
2025-08-08 14:32:00 +0200

    v134 - 1

---
+++ src/pages/35cbeafe-0f58-45d7-b5d9-65a49ea884a2.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import app from '@/_front/main.js';
+
+// eslint-disable-next-line no-undef
+import element_b783dc65_d528_4f74_8c14_e27c934c39b1 from '@/components/elements/element-b783dc65-d528-4f74-8c14-e27c934c39b1/src/wwElement.vue';
+import element_d7904e9d_fc9a_4d80_9e32_728e097879ad from '@/components/elements/element-d7904e9d-fc9a-4d80-9e32-728e097879ad/src/wwElement.vue';
+
+// eslint-disable-next-line no-undef
+app.component('wwobject-b783dc65-d528-4f74-8c14-e27c934c39b1', element_b783dc65_d528_4f74_8c14_e27c934c39b1);
+app.component('wwobject-d7904e9d-fc9a-4d80-9e32-728e097879ad', element_d7904e9d_fc9a_4d80_9e32_728e097879ad);
+
+// eslint-disable-next-line no-undef
+import section_99586bd3_2b15_4d6b_a025_6a50d07ca845 from '@/components/sections/section-99586bd3-2b15-4d6b-a025-6a50d07ca845/src/wwSection.vue';
+
+// eslint-disable-next-line no-undef
+app.component('section-99586bd3-2b15-4d6b-a025-6a50d07ca845', section_99586bd3_2b15_4d6b_a025_6a50d07ca845);
