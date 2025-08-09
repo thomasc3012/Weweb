f4a1fc2af711d33627a1e8800d15fd0c1b55eb08
Thomas Van epen (via WeWeb) <thomasve@live.nl>
2025-08-09 09:32:31 +0200

    v7 - 1

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 6;
+const version = 7;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
