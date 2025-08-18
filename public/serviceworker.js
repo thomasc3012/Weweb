472a490bcafcc346798d66f4668e9bf027aed749
Thomas Van epen (via WeWeb) <thomasve@live.nl>
2025-08-18 21:41:37 +0200

    v212 - 1

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 211;
+const version = 212;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
--- src/components/elements/element-6145eb60-0af8-4e52-bcc6-dc0f6743654e/src/editor/useEditorHint.js
+++
@@ -1,6 +0,0 @@
-import { provide, ref } from 'vue';
-
-export default function useEditorHint() {
-    provide('_wwSelect:isInSelect', ref(true));
-    return {};
-}
