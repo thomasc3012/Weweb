5605559e98c08350f55a7475e3449976ecb406c0
Thomas Van epen (via WeWeb) <thomasve@live.nl>
2025-08-28 20:42:39 +0200

    v1 - 1

---
+++ src/pages/97a088c2-e20c-41a1-be2f-ec26470b3bbe.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import app from '@/_front/main.js';
+
+// eslint-disable-next-line no-undef
+import element_69d0b3ef_b265_494c_8cd1_874da4aa1834 from '@/components/elements/element-69d0b3ef-b265-494c-8cd1-874da4aa1834/src/wwElement.vue';
+import element_6f8796b1_8273_498d_95fc_7013b7c63214 from '@/components/elements/element-6f8796b1-8273-498d-95fc-7013b7c63214/src/wwElement.vue';
+import element_aeb78b9a_6fb6_4c49_931d_faedcfad67ba from '@/components/elements/element-aeb78b9a-6fb6-4c49-931d-faedcfad67ba/src/wwElement.vue';
+import element_d7904e9d_fc9a_4d80_9e32_728e097879ad from '@/components/elements/element-d7904e9d-fc9a-4d80-9e32-728e097879ad/src/wwElement.vue';
+
+// eslint-disable-next-line no-undef
+app.component('wwobject-69d0b3ef-b265-494c-8cd1-874da4aa1834', element_69d0b3ef_b265_494c_8cd1_874da4aa1834);
+app.component('wwobject-6f8796b1-8273-498d-95fc-7013b7c63214', element_6f8796b1_8273_498d_95fc_7013b7c63214);
+app.component('wwobject-aeb78b9a-6fb6-4c49-931d-faedcfad67ba', element_aeb78b9a_6fb6_4c49_931d_faedcfad67ba);
+app.component('wwobject-d7904e9d-fc9a-4d80-9e32-728e097879ad', element_d7904e9d_fc9a_4d80_9e32_728e097879ad);
+
+// eslint-disable-next-line no-undef
+import section_99586bd3_2b15_4d6b_a025_6a50d07ca845 from '@/components/sections/section-99586bd3-2b15-4d6b-a025-6a50d07ca845/src/wwSection.vue';
+
+// eslint-disable-next-line no-undef
+app.component('section-99586bd3-2b15-4d6b-a025-6a50d07ca845', section_99586bd3_2b15_4d6b_a025_6a50d07ca845);
